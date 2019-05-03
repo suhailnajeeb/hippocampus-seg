789f35579d8d29f9a4cd2625de1b85800bee1a55
suhailnajeeb <suhailnajeeb19@gmail.com>
2019-05-03 19:19:17 +0600

    completed image display

--- task3.py
+++ task3.py
@@ -5,12 +5,31 @@ fileName = 'hippocampus_001.nii.gz'
 
 import os
 scanPath = os.path.join(img_fldr,fileName)
-labelPath = os.path.join(img_fldr,fileName)
+labelPath = os.path.join(label_fldr,fileName)
 
-#sliceNo = 10
+sliceNo = 10
 
 import nibabel as nib
 
-scan = nib.load(scanPath)
-data = scan.get_fdata()
+scanImg = nib.load(scanPath)
+dataImg = scanImg.get_fdata()
+img = dataImg[sliceNo, :, :]
+
+scanLabel = nib.load(labelPath)
+dataLabel = scanLabel.get_fdata()
+label = dataLabel[sliceNo, :, :]
+
+import matplotlib.pyplot as plt
+
+plt.figure()
+plt.subplot(1,2,1)
+plt.imshow(img)
+plt.title("MRI Scan")
+
+plt.subplot(1,2,2)
+plt.imshow(label)
+plt.title("Label")
+
+plt.show()
+
 
