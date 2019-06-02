94fc600ab9c533138baed91c2c89d52f69526ffc
suhailnajeeb <suhailnajeeb19@gmail.com>
2019-06-02 20:02:50 +0600

    update utilsDb resize funcion

--- utilsDb.py
+++ utilsDb.py
@@ -2,6 +2,6 @@
 
 import cv2
 
-def resize(img,xsize,ysize):
-    return cv2.resize(img, (xsize,ysize) , interpolation =cv2.INTER_CUBIC)
+def resize(img,rows,cols):
+    return cv2.resize(img, (rows,cols) , interpolation =cv2.INTER_CUBIC)
 
