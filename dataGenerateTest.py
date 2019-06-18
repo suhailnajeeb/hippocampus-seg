352a15f7300b1c8c277dfe56fdf9640fdc34254f
suhailnajeeb <suhailnajeeb19@gmail.com>
2019-06-18 20:58:57 +0600

    Cleanup

--- dataGenerateTest.py
+++ dataGenerateTest.py
@@ -1,72 +1,53 @@
 import nibabel as nib
 import glob
 import numpy as np
+import h5py
 
-scanPath = './Task04_Hippocampus/imagesTr/*.gz'
-labelPath = './Task04_Hippocampus/labelsTr/*.gz'
-
-#axis = 'X'
-
-scans = glob.glob(scanPath)
-labels = glob.glob(labelPath)
-
-scan = scans[0]
-label = labels[0]
-
-img = nib.load(scan)
-data = img.get_fdata()
-
-mask = nib.load(label)
-maskData = mask.get_fdata()
-
-size = [35,50,35]
-
-xsize, ysize, zsize = size
-'''
-xsize = 35
-ysize = 50
-zsize = 35
-'''
-# take slices along the yz plane:
-'''
-slices = []
-
-n = data.shape[0]
-for i in range(n):
-    slices.append(data[i,:,:])
-'''
-
-
-thresh = 4000
+# Utility Imports
 
 from utilsDb import capScan
 from utilsDb import normalize
 from utilsDb import return2DslicesAsList
 from utilsDb import resizeStack
 
+### global parameters ###
 
+size = [35,50,35]
+thresh = 4000
+plane = 'xy'
 
-#normalized = normalize(capped,thresh)
+scanName = 'hippocampus_001.nii.gz'
 
-# todo : modify the normalize function to normalize a single image. 
-# turns out i do not need to modify it. 
+scanPath = './Task04_Hippocampus/imagesTr/' + scanName
+labelPath = './Task04_Hippocampus/labelsTr/' + scanName
 
+#scans = glob.glob(scanPath)
+#labels = glob.glob(labelPath)
 
-capped = capScan(data,thresh)
-zx = return2DslicesAsList(capped,'zx')
-resized = resizeStack(zx,'zx',size)
+#scan = scans[0]
+#label = labels[0]
 
-maskzx = return2DslicesAsList(maskData, 'zx')
-maskResized = resizeStack(maskzx,'zx',size)
+###################### for a single image ##################################
+
+img = nib.load(scanPath)
+imgData = img.get_fdata()
+
+mask = nib.load(labelPath)
+maskData = mask.get_fdata()
+
+capped = capScan(imgData,thresh)
+slices = return2DslicesAsList(capped,plane)
+scanResized = resizeStack(slices,plane,size)
+
+masks = return2DslicesAsList(maskData, plane)
+maskResized = resizeStack(masks,plane,size)
 
 # todo: store all the images of a scan inlcuding mask in an h5 file.
 
 # todo: apply compression and formatting to dataset
 
-import h5py
-
-nimages = len(resized)
-shape = (nimages,) + resized[0].shape
+nimages = len(scanResized)
+shape = (nimages,) + scanResized[0].shape
 
 h5file = h5py.File("./data/testfile.h5","w")
 h5file.create_dataset("image",shape)
@@ -74,8 +55,8 @@ h5file.create_dataset("mask",(nimages,) + shape)
 
 didx = 0
 
-for img in resized:
-    h5file["image"][didx] = img
+for img in scanResized:
+    h5file["image"][didx] = normalize(img,thresh)
     didx = didx + 1
 
 didx = 0
@@ -83,16 +64,10 @@ for masks in maskResized:
     h5file["mask"][didx] = masks
     didx = didx + 1
 
-#h5file["image"][...] = singleSlice
-#h5file["mask"][...] = singleMask
-
 h5file.close()
 
-import numpy as np
+######################### ends here ############################
 
-zx = np.asarray(resized)
-print(zx.shape)
- 
 # todo: Study the standard deviation and distribution of the dataset
 
 # todo: Resize Scan in all dimensions
