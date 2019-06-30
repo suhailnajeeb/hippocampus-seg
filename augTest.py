3a2ecc41c0b0252c8aa881f1a6ff237c947adbbd
Suhail Najeeb <suhailnajeeb19@gmail.com>
2019-06-30 17:19:14 +0600

    bugfix : combine masks

--- augTest.py
+++ augTest.py
@@ -1,10 +1,11 @@
 from utilsDb import distort_img
-from utilsDb import distort
+#from utilsDb import distort
 
 import nibabel as nib
 import glob
 import numpy as np
 import matplotlib.pyplot as plt
+from keras.utils import to_categorical
 
 scanID = "hippocampus_001"
 
@@ -18,6 +19,10 @@ scan0 = scan[20]
 mask = nib.load(labelPath)
 maskData = mask.get_fdata()
 mask0 = maskData[20]
+
+maskCat = to_categorical(mask0)
+mask1 = maskCat[:,:,0]
+mask0 = 1-mask1
 
 data = [scan0,mask0]
 scan_distorted, mask_distorted = distort_img(data)
@@ -35,8 +40,12 @@ plt.subplot(2,2,3)
 plt.imshow(scan_distorted)
 plt.title("Distorted Scan")
 
+mask_distorted = np.round(mask_distorted)
+
 plt.subplot(2,2,4)
 plt.imshow(mask_distorted)
 plt.title("Distorted Label")
 
 plt.show()
+
+#https://towardsdatascience.com/full-stack-deep-learning-steps-and-tools-a21eda6227b1
