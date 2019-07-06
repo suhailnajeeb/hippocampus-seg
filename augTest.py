8e337fb4bb8a2c2a34fe42eeb8ca30863ca45f5f
Suhail Najeeb <suhailnajeeb19@gmail.com>
2019-07-06 10:50:39 +0600

    Cleanup

--- augTest.py
+++ augTest.py
@@ -2,7 +2,7 @@ from utilsDb import distort_img
 #from utilsDb import distort
 
 import nibabel as nib
-import glob
+#import glob
 import numpy as np
 import matplotlib.pyplot as plt
 from keras.utils import to_categorical
@@ -21,28 +21,28 @@ maskData = mask.get_fdata()
 mask0 = maskData[20]
 
 maskCat = to_categorical(mask0)
-mask1 = maskCat[:,:,0]
+mask1 = maskCat[:, :, 0]
 mask0 = 1-mask1
 
-data = [scan0,mask0]
+data = [scan0, mask0]
 scan_distorted, mask_distorted = distort_img(data)
 
 plt.figure()
-plt.subplot(2,2,1)
+plt.subplot(2, 2, 1)
 plt.imshow(scan0)
 plt.title("MRI Scan")
 
-plt.subplot(2,2,2)
+plt.subplot(2, 2, 2)
 plt.imshow(mask0)
 plt.title("Label")
 
-plt.subplot(2,2,3)
+plt.subplot(2, 2, 3)
 plt.imshow(scan_distorted)
 plt.title("Distorted Scan")
 
 mask_distorted = np.round(mask_distorted)
 
-plt.subplot(2,2,4)
+plt.subplot(2, 2, 4)
 plt.imshow(mask_distorted)
 plt.title("Distorted Label")
 
